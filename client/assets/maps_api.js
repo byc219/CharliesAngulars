b6cf5e4dd601a2f5e34af443adc7165669a5ca63
Ben Chen <coben88@gmail.com>
2016-05-20 15:48:59 -0700

    (file) multiple small changes for compatablity

--- client/assets/maps_api.js
+++ client/assets/maps_api.js
@@ -4,27 +4,6 @@
 var map;
 var infowindow;
 
-function initMap() {
-
-
-    var pyrmont = {
-        lat: -33.867,
-        lng: 151.195
-    };
-
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: pyrmont,
-        zoom: 15
-    });
-
-    infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-        location: pyrmont,
-        radius: 500,
-        type: ['park']
-    }, callback);
-}
 
 function callback(results, status) {
     if (status === google.maps.places.PlacesServiceStatus.OK) {
@@ -55,7 +34,7 @@ function handleAddress(input) {
     httpGetAsync('https://maps.googleapis.com/maps/api/geocode/json?address=' + formattedInput + '&key=AIzaSyAvP71A4zQ3bBjri75-1y6AaLP3s-JfNO0', handleLocation);
 
 }
-handleAddress('Death Valley');
+//handleAddress('Death Valley');
 
 function handleLocation(input) {
 
@@ -63,20 +42,20 @@ function handleLocation(input) {
 
 
 
-    var pyrmont = {
+    var location = {
         lat: inputJSON.results[0].geometry.location.lat,
         lng: inputJSON.results[0].geometry.location.lng
     };
 
     map = new google.maps.Map(document.getElementById('map'), {
-        center: pyrmont,
+        center: location,
         zoom: 15
     });
 
     infowindow = new google.maps.InfoWindow();
     var service = new google.maps.places.PlacesService(map);
     service.nearbySearch({
-        location: pyrmont,
+        location: location,
         radius: 500,
         type: ['park']
     }, callback);
